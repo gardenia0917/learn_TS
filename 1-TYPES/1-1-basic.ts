7f83fa38917a2ae44cf5b92eee929f5c5780abcb
gardenia1109 <gardenia1109@naver.com>
2022-04-10 20:32:23 +0900

    type

--- 1-TYPES/1-1-basic.ts
+++ 1-TYPES/1-1-basic.ts
@@ -27,4 +27,39 @@
     let person: null; // 💩
     let person2: string | null;
 
+    //unknown  💩 왠만하면 쓰지 말자
+    let notSure: unknown = 0;
+    notSure = 'he';
+    notSure = true;
+
+    // any 💩
+    let anything: any = 0;
+    anything = 'hello'
+
+    // void 함수에서 아무것도 리턴하지 않는 경우 void
+    // 생략가능 (회사 마다 다름)
+    function print():void {
+        console.log('hello');
+        return;
+    }
+    let unusable: void = undefined // 💩
+
+    // never 
+    /*  나는 리턴할 계획이 없으니 그점을 감안해서 코딩해!
+    함수에서 절대 리턴되지 않는 경우에
+    그것을 명시하기 위해 쓰임 */
+    function throwError(message: string): never {
+        // message -> server (log)
+        throw new Error(message); 
+        while (true) {}
+    }
+    let neverEnding: never; // 💩
+
+    // object
+    let obj: object; // 💩
+    function acceptSomeObject(obj: object) {
+        
+    }
+    acceptSomeObject({ name: 'ellie' });
+    acceptSomeObject({animal: 'cat'})
 }
