347847f56be937b30d262f4e64e2d77db79e0df8
gardenia1109 <gardenia1109@naver.com>
2022-04-11 01:32:37 +0900

    type-projects-quiz

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,17 +20,32 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  function printLoginState(result: ResourceLoadState) {
-    if (result.state === 'loading') {
-      console.log(`👀 loading...`);
-    } else if (result.state === 'success') {
-      console.log(`😃 ${result.response.body}`);
-    } else if (result.state === 'fail') {
-      console.log(`😱 ${result.reason}`);
-    } else {
-      console.log('Error');
+  // function printLoginState(result: ResourceLoadState) {
+  //   if (result.state === 'loading') {
+  //     console.log(`👀 loading...`);
+  //   } else if (result.state === 'success') {
+  //     console.log(`😃 ${result.response.body}`);
+  //   } else if (result.state === 'fail') {
+  //     console.log(`😱 ${result.reason}`);
+  //   } else {
+  //     console.log('Error');
+  //   }
+  // }
+  
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case 'loading':
+        console.log(`👀 loading...`);
+        break;
+      case 'success':
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case 'fail':
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`)
     }
-    
   }
 
   printLoginState({ state: 'loading' }); // 👀 loading...
