61ccdc3cddbf5336d79f0627222ef5806628847e
gardenia1109 <gardenia1109@naver.com>
2022-04-15 15:40:49 +0900

    get set

---
+++ 3-oop/3-3-encapsulation.ts
@@ -0,0 +1,63 @@
+{
+    type CoffeeCup = {
+        shots: number;
+        hasMilk: boolean;
+    }
+    // public 보편적, 외부에서 다 볼 수 있는 공개적
+    // private 외부에서 볼수 없고 접근할 수 없음
+    // protected  상속할때 외부에서 접근 안되지만 상속한 자식 클래스에서는 접근가능
+    class CoffeeMaker {
+        private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+        private coffeeBeans: number = 0; // instance (object) level
+        
+        private constructor(coffeeBeans:number) {
+            this.coffeeBeans = coffeeBeans;
+        }
+
+        static makeMachine(coffeeBeans: number): CoffeeMaker {
+            return new CoffeeMaker(coffeeBeans)
+        }
+
+        fillCoffeeBeans(beans: number) {
+            if (beans < 0) {
+                throw new Error('value for beans should be greater than 0')
+            }
+            this.coffeeBeans += beans;
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+            if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+            throw new Error('Not enough coffee beans!')
+        }
+        this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+        return {
+            shots,
+            hasMilk:false
+        }
+    }
+    }
+
+    const maker = CoffeeMaker.makeMachine(32);
+    maker.fillCoffeeBeans(32);
+
+    class User {
+        get fullName(): string {
+            return `${this.firstName} ${this.lastName}`
+        }
+        private internalAge = 4;
+        get age(): number {
+            return this.internalAge;
+        }
+        set age(num: number) {
+            this.internalAge = num;
+        }
+        constructor(private firstName: string,private lastName: string) {
+        }
+    }
+    const user = new User('Steve', 'Jobs');
+    console.log(user.fullName);
+    
+    user.firstName = 'Sunny'
+    console.log(user.fullName);
+    
+}
