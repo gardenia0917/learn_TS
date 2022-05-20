c388d15e44f1d32efce7051cc59ba931b198766d
gardenia1109 <gardenia1109@naver.com>
2022-05-21 06:41:10 +0900

    generic-class

---
+++ 5-generic/5-2-class.ts
@@ -0,0 +1,22 @@
+// either: a or b
+interface Either<L, R> {
+  left: () => L;
+  right: () => R;
+}
+
+class SimpleEither<L, R> implements Either<L, R> {
+  constructor(private leftValue: L, private rightValue: R) {}
+
+  left(): L {
+    return this.leftValue;
+  }
+  right(): R {
+    return this.rightValue;
+  }
+}
+
+const either = new SimpleEither(4, 6);
+either.left; // 4
+either.right; // 6
+
+const best = new SimpleEither({ name: "elle" }, "hello");
